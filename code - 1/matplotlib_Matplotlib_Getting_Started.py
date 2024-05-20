a5a280e9fe3ce9b3b4ba7596fcb6aa68e44db4f7
Anumit Jooloor <anumit@gmail.com>
2024-05-20 04:46:31 -0700

    Add files

---
+++ code - 1/matplotlib_Matplotlib_Getting_Started.py
@@ -0,0 +1,75 @@
+#   python3 pandas_Read_CSV.py
+"""
+python3 matplotlib_Matplotlib_Getting_Started.py
+"""
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+import pandas as pd
+import numpy as np
+ 
+
+
+print("Matplotlib")
+print("************************************")
+
+import matplotlib
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+
+
+
+
+"""
+x = ["apple", "banana", "cherry"] 	list 	
+x = ("apple", "banana", "cherry") 	tuple
+x = {"name" : "John", "age" : 36} 	dict 	
+x = {"apple", "banana", "cherry"} 	set
+"""
+
+"""
+x = list(("apple", "banana", "cherry")) 	list 	
+x = tuple(("apple", "banana", "cherry")) 	tuple
+x = dict(name="John", age=36) 	            dict 	
+x = set(("apple", "banana", "cherry")) 	    set
+"""
+
---
+++ code - 1/matplotlib_Matplotlib_Plotting.py
@@ -0,0 +1,85 @@
+#   python3 pandas_Read_CSV.py
+"""
+python3 matplotlib_Matplotlib_Plotting.py
+"""
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+import pandas as pd
+import numpy as np
+
+
+print("Matplotlib")
+print("************************************")
+
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+print("---------------------------------------------------")
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints, 'o')
+plt.show()
+
+
+print("---------------------------------------------------")
+
+xpoints = np.array([1, 2, 6, 8])
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+ 
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+print("---------------------------------------------------")
+
+
+
+
+
+
+"""
+x = ["apple", "banana", "cherry"] 	list 	
+x = ("apple", "banana", "cherry") 	tuple
+x = {"name" : "John", "age" : 36} 	dict 	
+x = {"apple", "banana", "cherry"} 	set
+"""
+
+"""
+x = list(("apple", "banana", "cherry")) 	list 	
+x = tuple(("apple", "banana", "cherry")) 	tuple
+x = dict(name="John", age=36) 	            dict 	
+x = set(("apple", "banana", "cherry")) 	    set
+"""
+
